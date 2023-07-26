65faa51ba74767f43c54f90c4ca6fb35761ab4ea
Bruno Duarte <contato.bduaart@gmail.com>
2023-07-26 15:55:32 -0300

    feat: update validate branch shell

--- .husky/validate-branch.sh
+++ .husky/validate-branch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Extrai o nome da branch remota a ser criada
 remote_branch=$(git rev-parse --abbrev-ref HEAD)
-# Verifica se o nome da branch segue o padrão 'tipo/string-qualquer'
+# Verifica se o nome da branch segue o padrão 'tipo/funcionalidade'
 if echo "$remote_branch" | grep -qE '^(bugfix|feature|hotfix|improvement)/[^/]+$'; then
     exit 0
 fi
@@ -9,5 +9,5 @@ fi
 if echo "$remote_branch" | grep -qE '^release-[0-9]+\.[0-9]+\.[0-9]+$'; then
     exit 0
 fi
-echo "Erro: o nome da branch não segue nenhum dos padrões suportados."
+echo "Erro: o nome da branch não segue nenhum dos padrões suportados. (bugfix/feature/hotfix/improvement) ou (release-X.X.X)."
 exit 1
