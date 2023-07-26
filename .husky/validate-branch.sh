a71ed924d2c7c7054693e5294a61cce897e47ebf
Bruno Duarte <contato.bduaart@gmail.com>
2023-07-26 15:53:07 -0300

    test: test

---
+++ .husky/validate-branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Extrai o nome da branch remota a ser criada
+remote_branch=$(git rev-parse --abbrev-ref HEAD)
+# Verifica se o nome da branch segue o padrão 'tipo/string-qualquer'
+if echo "$remote_branch" | grep -qE '^(bugfix|feature|hotfix|improvement)/[^/]+$'; then
+    exit 0
+fi
+# Verifica se o nome da branch segue o padrão 'release-X.X.X'
+if echo "$remote_branch" | grep -qE '^release-[0-9]+\.[0-9]+\.[0-9]+$'; then
+    exit 0
+fi
+echo "Erro: o nome da branch não segue nenhum dos padrões suportados."
+exit 1
