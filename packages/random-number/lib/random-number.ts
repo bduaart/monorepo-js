96c2773bd5434cb78c8bf62a063cdd74cee3f9e7
Bruno Duarte <contato.bduaart@gmail.com>
2023-07-26 16:06:46 -0300

    feat: add build script

--- packages/random-number/lib/random-number.ts
+++ packages/random-number/lib/random-number.ts
@@ -1,10 +1,7 @@
 "use strict";
 
 // 1
-function randomNumber(min, max) {
+export function randomNumber(min, max) {
   const num = Math.floor(Math.random() * (max - min) + min);
   return num;
 }
-
-// 2
-module.exports = randomNumber;
